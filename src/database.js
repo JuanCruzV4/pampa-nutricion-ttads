a4ce857d140bcd28dc5b9419d1f4e1ab3ec93513
JuanCruzV4 <juancruz.vazquez87@hotmail.com>
2022-05-26 02:26:32 -0300

    Controller

--- src/database.js
+++ src/database.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const {MONGODB_URI} = require ('./config');
+console.log(process.env.MONGODB_URI);
 
 (async () => {
     try {
-        const db = await mongoose.connect("mongodb://localhost:27017/pampa-db");
+        const db = await mongoose.connect(process.env.MONGODB_URI);
         console.log('DB connected to', db.connection.name);
     }catch (error){
         console.error(error);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 require ('./database');
-//import app from "./app";
+
 
 app.listen(3000);
 console.log('Server on port',3000);
